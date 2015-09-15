d2dc2aaec587298d5054973ee4406ff4f1ee3724
nicholashaynes1 <nicholashaynes1@gmail.com>
2015-09-15 13:33:16 -0600

    The name has been changed, and the ability to change the name

--- src/controller/marshmallowmonster/MonsterController.java
+++ src/controller/marshmallowmonster/MonsterController.java
@@ -2,11 +2,13 @@ package controller.marshmallowmonster;
 
 import creating.monster.model.MarshmallowMonster;
 import marshmallow.view.MonsterDisplay;
+import java.util.Scanner;
 
 public class MonsterController
 {
 	private MarshmallowMonster NickMonster;
 	private MonsterDisplay myDisplay;
+	private Scanner monsterScanner;
 	
 	public MonsterController()
 	{
@@ -18,11 +20,22 @@ public class MonsterController
 		boolean HasBellyButton = false;
 		myDisplay = new MonsterDisplay();
 		NickMonster = new MarshmallowMonster(Name, Eyes, Nose, Legs, Hair, HasBellyButton);
+		
+		monsterScanner = new Scanner(System.in);
 	}		
 	
 	
 	public void start()
 	{
 		myDisplay.displayinfo(NickMonster.toString());
+		askQuestions();
+		myDisplay.displayinfo("Updated monster info" + NickMonster.toString());
+	}
+	
+	private void askQuestions()
+	{
+		System.out.println("I want a new name, Type one out?");
+		String newMonsterName = monsterScanner.next();
+		NickMonster.setmonsterName(newMonsterName);
 	}
 }
